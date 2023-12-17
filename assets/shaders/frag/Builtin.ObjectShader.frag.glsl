498f51541217eeb7d7067c97234a3b0e193fc42a
raustin9 <raustin9@vols.utk.edu>
2023-12-17 17:41:52 -0500

    basic interpolation

--- assets/shaders/frag/Builtin.ObjectShader.frag.glsl
+++ assets/shaders/frag/Builtin.ObjectShader.frag.glsl
@@ -2,7 +2,9 @@
 #extension GL_ARB_separate_shader_objects : enable
 
 layout (location = 0) out vec4 out_color;
+layout (location = 0) in vec3 in_position;
 
 void main() {
-    out_color = vec4(0.2, 0.4, 0.6, 1.0);
+    // out_color = vec4(0.8, 0.3, 0.7, 1.0);
+    out_color = vec4(in_position, 1.0);
 }
